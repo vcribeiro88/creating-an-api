704d7f8a206d54acb451c70b206e2d0b01f4ff0c
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2020-12-07 08:59:16 -0400

    alteração na porta do servidor

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require('./config/express')();
 const port = app.get('port');
 
 //RODANDO A APLICAÇÃO NA PORTA SETADA
-app.listen(process.env.APP_URL, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log(`Servidor rodando`)
 });
