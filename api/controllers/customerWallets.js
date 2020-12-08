0504e3826606e0ab865b9a8b2b27570701ae8416
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2020-12-08 17:55:22 -0400

    Alteração na requisiçaão post

--- api/controllers/customerWallets.js
+++ api/controllers/customerWallets.js
@@ -7,7 +7,7 @@ module.exports = () => {
     controller.listCustomerWallets = (req, res) => res.status(200).json(customerWalletsDB);
 
     controller.saveCustomersWallets = (req, res) => {
-        customerWalletsMock.data.push({
+        customerWalletsDB.data.push({
             id: uuidv4(),
             name: req.body.name,
             occupation: req.body.occupation,
